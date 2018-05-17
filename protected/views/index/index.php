71f16822fd8cbfd803324faf1205e6fd351e20ed
NereusLee <dreizack086@hotmail.com>
2018-05-17 23:14:01 +0800

    页面渲染有问题

---
+++ protected/views/index/index.php
@@ -0,0 +1,26 @@
+
+<head>
+    <meta charset=utf-8>
+    <meta name=viewport content='width=device-width,initial-scale=1'>
+    <title>daydaycook</title>
+    <script>
+    (function() {
+        let h = document.documentElement;
+        let w = h.getBoundingClientRect().width;
+        h.style.fontSize = w / 15 + 'px'
+    })()
+    </script>
+    <link rel=stylesheet href='http://at.alicdn.com/t/font_589980_ucxzuz43sku1h5mi.css'>
+    <link href='<?php echo Yii::app()->request->baseUrl?>/assets/index/css/app.6b0f1e2d7ddfddd912f881ebd32111ad.css' rel=stylesheet>
+</head>
+
+<body>
+    <div id=app></div>
+    <script type='text/javascript' src='<?php echo Yii::app() ->request -> baseUrl?>/assets/index/js/manifest.3ad1d5771e9b13dbdad2.js' >
+    </script>
+    <script type='text/javascript' src='<?php echo Yii::app() ->request -> baseUrl?>/assets/index/js/vendor.423d68892c1c311fb0cd.js' >
+    </script>
+    <script type='text/javascript' src='<?php echo Yii::app() ->request -> baseUrl?>/assets/index/js/app.aa4a9d84776b230a931f.js' >
+    </script>
+</body>
+
